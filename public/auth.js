1d051f5ae2109d57acac44c4cc2b2252a0efb5c1
f20230379-droid <f20230379@hyderabad.bits-pilani.ac.in>
2026-07-26 16:01:30 +0530

    Add Supabase client

---
+++ public/auth.js
@@ -0,0 +1,13 @@
+const supabaseUrl = "sb_publishable_HOXyV8I8YIFNm9l5zI1EJQ_9UajP_gO";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3c2xibWdmcm9lanRreHFueGhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5OTQ5NTYsImV4cCI6MjEwMDU3MDk1Nn0.ZQaJheDyBm8n_E6rpQanB84gHKypLa2Ldg8RMiXrGws";
+
+const supabase = window.supabase.createClient(
+    supabaseUrl,
+    supabaseAnonKey
+);
+
+// Make it available to the rest of the app
+window.supabaseClient = supabase;
+
+console.log("✅ Connected to Supabase");
+    
